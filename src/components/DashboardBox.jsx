af316a4bdf4e2a07f08123f8ec55ab09a611db70
ziad badry <badry8977@gmail.com>
2026-01-24 16:38:23 +0200

    designing dashboard boxes and adding istablet hook

---
+++ src/components/DashboardBox.jsx
@@ -0,0 +1,21 @@
+import { useIsTablet } from "../hooks/isTablet"
+
+export default function DashboardBox({label,Icon,stats='2000'}){
+    const isTablet=useIsTablet();
+    return (
+        <>
+            <div className="basis-1/4 flex p-4 justify-between h-1/8 rounded-xl items-center bg-white ">
+            <div>
+            <p className="text-gray-600 text-[clamp(11px,1.2vw,14px)] whitespace-nowrap ">{label}</p>
+            <p className="font-bold">{stats}</p>
+            </div>
+            {!isTablet && 
+            <div className="bg-[#4fd1c5] p-3 rounded-xl">
+            <Icon color={'white'} size={30}></Icon>
+            </div>
+            
+            }
+            </div>
+        </>
+    )
+}
