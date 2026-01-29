c9990b5c786de5ff26a3f0e857c639c83a2302b3
ziad badry <badry8977@gmail.com>
2026-01-29 18:00:41 +0200

    added simplebarchart and indexlinechart  components

--- src/components/DashboardBoxes.jsx
+++ src/components/DashboardBoxes.jsx
@@ -1,26 +1,12 @@
 import { Wallet, Globe, FileText, ShoppingCart } from "lucide-react";
 import DashboardBox from "./DashboardBox";
+import BoxesLayout from "./BoxesLayout";
 
-export default function Dashboardboxex(){
-    const Icons_mapping=[
-        {label:"Today\'s money",
-        Icon:Wallet},
-        {label:"Today\'s Users",
-        Icon:Globe},
-        {label:'New clients',
-        Icon:FileText},
-       { label:'Total sales',
-    Icon:ShoppingCart},
-    ]
+export default function Dashboardboxex({list}){
+
     return (
         <>
-            <div className="flex w-full flex-wrap justify-between gap-4 sm:gap-6 md:gap-8 lg:gap-10">
-                {Icons_mapping.map((e, index) => 
-                    <DashboardBox key={index} label={e.label} Icon={e.Icon}></DashboardBox>
-                )}
-
-
-            </div>
+            <BoxesLayout list={list}></BoxesLayout>
         </>
     )
 }
