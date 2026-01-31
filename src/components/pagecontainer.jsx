c1eddf9162d412e574f05d9b3abdf3303060f41d
ziad badry <badry8977@gmail.com>
2026-01-31 19:00:11 +0200

    imporving PageContainer reusability

--- src/components/pagecontainer.jsx
+++ src/components/pagecontainer.jsx
@@ -1,8 +1,15 @@
+import Sidebar from "./sidebar";
+
 export default function Pagecontainer({children}){
     return (
         <>
             <div className="p-4 sm:p-6 md:p-8 flex flex-col  sm:flex-row gap-4 sm:gap-8 md:gap-12 lg:gap-20 ml-[60px] sm:ml-0">
+                <Sidebar></Sidebar>
+                
+                <div className='flex w-full flex-col gap-6 sm:gap-8 md:gap-4'>
                 {children}
+                </div>
+                
             </div>
         </>
     )
